b907f3a61eedd79bca1794bc4a733b4bad0eb068
alexey.papin <apapin@alfabank.ru>
2021-04-06 21:40:15 +0300

    Initial Commit

---
+++ WeatherHelper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherHelper
+//
+//  Created by Алексей Папин on 06.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
